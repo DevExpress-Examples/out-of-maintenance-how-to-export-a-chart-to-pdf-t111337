50a1bcfc7f0f6391609006947b05d3aac9c92a2c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to export a chart to PDF
+
+
+The following example demonstrates how to export the chart's data as a PDF document to a file.
+
+<br/>
+
+
---
+++ VB/ExportToPdf/MainWindow.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Windows
+Imports DevExpress.Xpf.Charts
+
+Namespace ExportToPdf
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Private PDFFile As String = "Output.pdf"
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim sizeMode As PrintSizeMode = PrintSizeMode.Stretch
+			chartControl.ExportToPdf(PDFFile, sizeMode)
+			Process.Start(PDFFile)
+		End Sub
+	End Class
+End Namespace
