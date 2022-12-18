f154d663748a13540de8540bb6322802a49e8f9b
DevExpressExampleBot <fakeEmail>
2022-12-18 18:52:22 +0400

    Source auto update [skip ci]

--- VB/ExportToPdf/MainWindow.xaml.vb
+++ VB/ExportToPdf/MainWindow.xaml.vb
@@ -1,22 +1,22 @@
-﻿Imports System.Diagnostics
-Imports System.IO
+Imports System.Diagnostics
 Imports System.Windows
 Imports DevExpress.Xpf.Charts
 
 Namespace ExportToPdf
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private PDFFile As String = "Output.pdf"
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Private PDFFile As String = "Output.pdf"
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim sizeMode As PrintSizeMode = PrintSizeMode.Stretch
-			chartControl.ExportToPdf(PDFFile, sizeMode)
-			Process.Start(PDFFile)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim sizeMode As PrintSizeMode = PrintSizeMode.Stretch
+            Me.chartControl.ExportToPdf(PDFFile, sizeMode)
+            Call Process.Start(PDFFile)
+        End Sub
+    End Class
 End Namespace
